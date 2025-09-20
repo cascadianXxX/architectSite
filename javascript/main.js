81fd8612fe6aada0df1698b835c475ccfd1f85ac
cadostropia <73354750+cadostropia@users.noreply.github.com>
2025-09-20 13:56:12 +0300

    new event

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,7 @@
-
 const image = document.getElementById('dr.alihankeskin_image');
 image.addEventListener('click', function() {
     window.location.href = 'https://www.uksek.org/_files/ugd/614b1f_b38b4d68ce964033a944fd048e388920.pdf#page=24.00';
 });
-
-
-
 const image2 = document.getElementById('dau_image');
 image2.addEventListener('click', function() {
     window.location.href = 'https://pr.emu.edu.tr/Documents/DAUdeGecenHafta/2024/12/20_Aralik_2024.pdf#page=2.40';
@@ -50,3 +46,10 @@ function closeZoom() {
         overlay.classList.remove("active-overlay");
     }
 }
+
+document.getElementById("img_1").addEventListener("click", function () {
+    window.location.href = this.dataset.link;
+});
+document.getElementById("img_2").addEventListener("click", function () {
+    window.location.href = this.dataset.link;
+});
